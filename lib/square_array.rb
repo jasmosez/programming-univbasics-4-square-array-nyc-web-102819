48d60f3f8a8bd9395cc7e5d9b646eddd3b5969c0
James Schaffer <jas.schaffer@gmail.com>
2019-09-11 02:57:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,13 @@
 def square_array(array)
+
   counter = 0
+  new_array = []
+  
   while array[counter] do
     new_array[counter] = array[counter] ** 2
     counter += 1
   end
+  
   return new_array
+
 end
